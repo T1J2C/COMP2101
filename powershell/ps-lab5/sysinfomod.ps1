17c8ba52ec76df8bca865bc1885d4afb1112d5fd
Thomas <thmscooper.1@gmail.com>
2021-12-14 04:03:04 -0500

    Modified SysInfo.ps1
    Modified version of sysinfo.ps1, all functions are stored in the module and are called by using parameters/if statements.

---
+++ powershell/ps-lab5/sysinfomod.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+Param(
+      [Parameter(Mandatory=$false)][switch]$System=$false,  #none of the parameters are mandatory, will call specific funtions if these parameters are selected
+      [Parameter(Mandatory=$false)][switch]$Disks=$false,
+      [Parameter(Mandatory=$false)][switch]$Network=$false           
+    )
+
+
+#If statements to determine which parameter has been used, and which functions should be run
+
+$all = 0
+If ($System) {
+    Get-CpuInfo
+    Get-OpSystem
+    Get-PhysMem
+    Get-VidCard
+    $all += 1		#added 'all' variable to check if a parameter was called
+}
+
+If ($Disks) {
+    Get-PhysDiskDrive
+    $all += 1
+}
+
+If ($Network) {
+    Get-NetAdapterConf
+    $all += 1
+}
+
+ if ($all -lt 1) {	#if 'all' is less than 1, meaning no parameters were used, all of the functions are called
+    Get-SysHardware
+    Get-OpSystem
+    Get-Processor
+    Get-PhysMem
+    Get-PhysDiskDrive
+    Get-NetAdapterConf
+    Get-VidCard
+    Get-CpuInfo
+    Get-MyDisks
+}
+    
